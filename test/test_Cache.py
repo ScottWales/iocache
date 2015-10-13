65ed09bd298fc55c35487eed4ffea9b8109ae424
Scott Wales <scott.wales@unimelb.edu.au>
2015-10-13 16:57:50 +1100

    Add a test

--- test/test_Cache.py
+++ test/test_Cache.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 """
-Copyright 2015 ARC Centre of Excellence for Climate Systems Science
+Copyright 2015 Scott Wales
 
 author: Scott Wales <scott.wales@unimelb.edu.au>
 
@@ -16,8 +16,23 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
+import pytest
 
-from scache.Cache import Cache
+from scache.Cache import Cache, trace_re
 
-def test_create():
-    c = Cache()
+@pytest.fixture(scope='session')
+def cache(tmpdir_factory):
+    cache_path = tmpdir_factory.mktemp('cache').strpath
+    return Cache(cache_path = cache_path)
+
+def test_parse_trace(tmpdir, cache):
+    a = tmpdir.join('a')
+    a.write('a')
+    a_path = a.strpath
+
+    b = tmpdir.join('b')
+    b.write('b')
+    b_path = b.strpath
+    
+    cache.parse_trace('open("%s", O_RDONLY) = 1'%a_path)
+    cache.parse_trace('open("%s", O_WRONLY) = 2'%b_path)
